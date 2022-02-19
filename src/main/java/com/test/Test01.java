c79075ad739c25d9db6c1eafd765a4574ac7eb8c
renyi <977713017@qq.com>
2022-02-19 16:52:07 +0800

    删除了主程序的执行代码

--- src/main/java/com/test/Test01.java
+++ src/main/java/com/test/Test01.java
@@ -9,15 +9,6 @@ import java.util.Set;
 
 public class Test01 {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        DeptService deptServiceImpl = (DeptService) classPathXmlApplicationContext.getBean("deptServiceImpl");
-        List<Map<String, Object>> maps = deptServiceImpl.selectDept8();
-        for (Map<String, Object> map : maps) {
-            Set<String> strings = map.keySet();
-            for (String s : strings) {
-                System.out.println(s);
-                System.out.println(map.get(s));
-            }
-        }
+
     }
 }
